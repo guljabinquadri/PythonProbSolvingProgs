097348ee31630b91f5a4963e79399d37a123a0de
Guljabin Quadri <78896565+guljabinquadri@users.noreply.github.com>
2021-03-20 22:19:14 +0530

    init

---
+++ BreakContiuneEg.py
@@ -0,0 +1,9 @@
+
+while(True):
+    inp = int(input("Enter a number\n"))
+    if(inp > 100):
+        print("Congo you have enterd a greater number")
+        break
+    else:
+        print("Try again\n")
+        continue
